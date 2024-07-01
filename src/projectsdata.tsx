418a4ead52243f72b7f5bb90dd27ddc51e7d3e5f
Tary Junior <111458237+tn-junior@users.noreply.github.com>
2024-07-01 14:46:39 -0300

    Up projectsdata.tsx

--- src/projectsdata.tsx
+++ src/projectsdata.tsx
@@ -1,44 +1,47 @@
-import professor from './assets/mock/professor.png';
-import orientador from './assets/mock/orientador.png';
-import pokedex from './assets/mock/pokedex.png';
-import flix from './assets/mock/flix.png';
+import fomezero from './assets/mock/fomezero.png';
+import jarvis from './assets/mock/jarvis.png';
+import cowrun from './assets/mock/cowrun.png';
+import stock from './assets/mock/stock.png';
+
+
+
 
 const ProjectsData = [
     {
         "id": 1,
-        "img": professor,
-        "title": "TCC - Professor",
+        "img": fomezero,
+        "title": "Fome Zero - DiagramaUML",
         "description": "Projeto desenvolvido por mim e pela minha equipe em 2021 para minha escola como TCC e estágio, com principal função dos professores aplicarem ocorrência nos alunos dentro da sala de aula através do celular.",
-        "tool": "React · Firebase",
-        "github": "https://github.com/ogabrielbarbosa/TCC-Professor",
-        "link": "https://tccprofessor.gabarbosa.com/"
+        "tool": "SpringBoot · Java · Azure ",
+        "github": "https://github.com/tn-junior/diagramaUML-Unicap.git",
+        //"link": "deploy"
     },
     {
         "id": 2,
-        "img": orientador,
-        "title": "TCC - Orientador",
+        "img": jarvis,
+        "title": "Jarvis  - Virtual Assistant",
         "description": "Projeto desenvolvido por mim e pela minha equipe em 2021 para minha escola como TCC e estágio, com principal função dos orientadores receberem, organizarem as ocorrências dos alunos da escola.",
-        "tool": "React · Firebase",
-        "github": "https://github.com/ogabrielbarbosa/TCC-Orientador",
-        "link": "https://tccorientador.gabarbosa.com/"
+        "tool": "JavaScript · HTML · CSS · WebAPI ",
+        "github": "https://github.com/tn-junior/jarvis-VA.git",
+        "link": "https://jarvis-2yushqxzb-tarynascimentos-projects.vercel.app/"
     },
     {
         "id": 3,
-        "img": pokedex,
-        "title": "Pokédex",
-        "description": "Pokédex para procurar pokémons pelo nome ou número, mostrando e filtrando dados referente ao pokémon selecionado.",
-        "tool": "React · TypeScript · PokeAPI",
-        "github": "https://github.com/ogabrielbarbosa/pokedex",
-        "link": "https://pokedex.gabarbosa.com/"
+        "img": cowrun,
+        "title": "Cow Run",
+        "description": "Jogo inspirado no Crossy Road.",
+        "tool": "JavaScript · Html ",
+        "github": "https://github.com/tn-junior/CowRun.git",
+        "link": "https://cow-run-xi.vercel.app/"
     },
     {
         "id": 4,
-        "img": flix,
-        "title": "Flix",
-        "description": "Plataforma de filmes em exibição, com acesso a informações do filme, como tempo de duração, gêneros, sinopse, entre outras coisas.",
-        "tool": "React · TheMovieDB",
-        "github": "https://github.com/ogabrielbarbosa/flix",
-        "link": "https://flix.gabarbosa.com/"
+        "img": stock,
+        "title": "Stock_Analysis",
+        "description": "Projeto análise de dados de acões solucões",
+        "tool": "Python",
+        "github": "https://github.com/tn-junior/stock_analysis.git",
+        "link": " deploy"
     }
 ]
 
