4df141513f663a5d657be9cf2991393c155a6369
tn-junior <juju2004.tary@gmail.com>
2025-01-06 14:16:21 -0300

    feat: ajustes

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -35,10 +35,11 @@ const About: React.FC<Props> = ({ toggleTheme }) => {
                 </Description>
                 <p>&bull; 
                 Desenvolvedor de Software com 2 anos de experiência, visando seguir carreira em desenvolvimento Full Stack, soluções de Machine Learning e Data Science, com foco em entregar soluções inovadoras alinhadas aos objetivos de negócio.</p>
-                <p>&bull; graduating in Internet Systems.</p>
-                <p>&bull; 2 year experience</p>
+                <p>&bull; Formado em Sistemas para Internet na Universidade Católica de Pernambuco.</p>
+                <p>&bull; 2 Anos de Experiência</p>
                {/* <p>&bull; <a className="link" href="https://www.efset.org/cert/KjKPY3" target="_blank" rel="noopener noreferrer">C1 Advanced English Level</a></p> */}
-                <p>&bull; Studying Cloud Solutions in AWS and Events-Driven Architecture</p>
+                <p>&bull; 
+                Estudando soluções em nuvem em AWS e arquitetura orientada a eventos</p>
 
                 <Description>
                     Estou sempre em busca de novos desafios, aprendendo tecnologias inovadoras e buscando formas de melhorar a eficiência e a escalabilidade dos projetos em que atuo.
